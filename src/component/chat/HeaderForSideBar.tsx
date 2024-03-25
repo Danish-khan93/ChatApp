0fb9bfcebf293b5f657782b9bf1c3fd695441514
Danish-khan93 <danishshoukat1994@gmail.com>
2024-03-26 03:47:02 +0500

    SIgnOut functionality with firebase

--- src/component/chat/HeaderForSideBar.tsx
+++ src/component/chat/HeaderForSideBar.tsx
@@ -1,19 +1,18 @@
-import { Box, Typography,Button } from "@mui/material";
+import { Box, Typography, Button } from "@mui/material";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import { FC } from "react";
+import { signOut } from "firebase/auth";
+import {auth} from "../../firebase"
 const HeaderForSideBar: FC = () => {
-
-const handleLogout =() =>{
-  console.log("logout");
-  
-}
-
-
   return (
     <Box className="flex justify-between items-center w-full">
-      <Box  className="flex justify-around gap-4">
+      <Box className="flex justify-around gap-4">
         <Box>
-          <Typography className="rounded-full" component={"img"} src="https://picsum.photos/50/50"></Typography>
+          <Typography
+            className="rounded-full"
+            component={"img"}
+            src="https://picsum.photos/50/50"
+          ></Typography>
         </Box>
         <Box>
           <Typography className="font-semibold">name</Typography>
@@ -21,7 +20,12 @@ const handleLogout =() =>{
         </Box>
       </Box>
       <Box className="flex items-center">
-        <Button className="bg-[#1b1b1b] w-full hover:bg-[#1b1b1b] text-[12px] text-[#fff] px-[10px]" onClick={handleLogout}>logOut</Button>
+        <Button
+          className="bg-[#1b1b1b] w-full hover:bg-[#1b1b1b] text-[12px] text-[#fff] px-[10px]"
+          onClick={()=>signOut(auth)}
+        >
+          logOut
+        </Button>
         <Typography>
           <NotificationsNoneIcon />
         </Typography>
